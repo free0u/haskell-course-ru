321f65d34e01aafab9db7a8ea63301d6792ba4b0
Anton Evdokimov <free0u@gmail.com>
2014-11-18 17:40:37 +0300

    Completed Primitive

--- ITMOPrelude/Primitive.hs
+++ ITMOPrelude/Primitive.hs
@@ -83,24 +83,39 @@ data Nat = Zero | Succ Nat deriving (Show,Read)
 
 natZero = Zero     -- 0
 natOne = Succ Zero -- 1
+nat2 = natOne +. natOne
+nat3 = nat2 +. natOne
+nat4 = nat3 +. natOne
+nat5 = nat4 +. natOne
+nat6 = nat5 +. natOne
+nat7 = nat6 +. natOne
+nat8 = nat7 +. natOne
+nat9 = nat8 +. natOne
 
 -- Сравнивает два натуральных числа
 natCmp :: Nat -> Nat -> Tri
-natCmp = undefined
+natCmp Zero Zero = EQ
+natCmp Zero (Succ _) = LT
+natCmp (Succ _) Zero = GT
+natCmp (Succ n) (Succ m) = natCmp n m
 
 -- n совпадает с m 
 natEq :: Nat -> Nat -> Bool
-natEq Zero     Zero     = True
-natEq Zero     (Succ _) = False
-natEq (Succ _) Zero     = False
-natEq (Succ n) (Succ m) = natEq n m
+natEq m n = case natCmp m n of
+  EQ -> True
+  _ -> False
 
 -- n меньше m
 natLt :: Nat -> Nat -> Bool
-natLt Zero     Zero     = False
-natLt Zero     (Succ m) = True
-natLt (Succ n) Zero     = False
-natLt (Succ n) (Succ m) = natLt n m
+natLt m n = case natCmp m n of
+  LT -> True
+  _ -> False
+
+natGt :: Nat -> Nat -> Bool
+natGt m n = case natCmp m n of
+  GT -> True
+  _ -> False
+
 
 infixl 6 +.
 -- Сложение для натуральных чисел
@@ -111,7 +126,10 @@ Zero     +. m = m
 infixl 6 -.
 -- Вычитание для натуральных чисел
 (-.) :: Nat -> Nat -> Nat
-n -. m = undefined
+m -. Zero = m
+n -. (Succ m) = case (n -. m) of
+  Zero -> Zero
+  Succ x -> x
 
 infixl 7 *.
 -- Умножение для натуральных чисел
@@ -121,83 +139,121 @@ Zero     *. m = Zero
 
 -- Целое и остаток от деления n на m
 natDivMod :: Nat -> Nat -> Pair Nat Nat
-natDivMod n m = undefined
+natDivMod _ Zero = undefined
+natDivMod n m = case natCmp n m of
+  LT -> Pair natZero n
+  _ -> let p = natDivMod (n -. m) m in
+    Pair (Succ $ fst p) $ snd p
 
 natDiv n = fst . natDivMod n -- Целое
 natMod n = snd . natDivMod n -- Остаток
 
 -- Поиск GCD алгоритмом Евклида (должен занимать 2 (вычислителельная часть) + 1 (тип) строчки)
 gcd :: Nat -> Nat -> Nat
-gcd = undefined
+gcd a Zero = a
+gcd a b = gcd b $ natMod a b
 
 -------------------------------------------
 -- Целые числа
 
 -- Требуется, чтобы представление каждого числа было единственным
-data Int = UNDEFINED deriving (Show,Read)
+data Int = Pos Nat | Neg Nat deriving (Show,Read)
 
-intZero   = undefined   -- 0
-intOne    = undefined     -- 1
-intNegOne = undefined -- -1
+intZero   = Pos natZero   -- 0
+intOne    = Pos natOne     -- 1
+intNegOne = Neg natZero -- -1
+int2 = Pos $ Succ $ Succ Zero
+int3 = Pos $ Succ $ Succ $ Succ Zero
+int4 = Pos $ Succ $ Succ $ Succ $ Succ Zero
+intn2 = Neg $ Succ Zero
+intn3 = Neg $ Succ $ Succ Zero
+intn4 = Neg $ Succ $ Succ $ Succ Zero
 
 -- n -> - n
 intNeg :: Int -> Int
-intNeg = undefined
+intNeg (Pos Zero) = Pos Zero
+intNeg (Pos (Succ x)) = Neg x
+intNeg (Neg x) = Pos $ Succ x
 
 -- Дальше также как для натуральных
 intCmp :: Int -> Int -> Tri
-intCmp = undefined
+intCmp (Neg _) (Pos _) = LT
+intCmp (Pos _) (Neg _) = GT
+intCmp (Pos m) (Pos n) = m `natCmp` n
+intCmp (Neg m) (Neg n) = n `natCmp` m
 
 intEq :: Int -> Int -> Bool
-intEq = undefined
+intEq m n = case m `intCmp` n of
+  EQ -> True
+  _ -> False
 
 intLt :: Int -> Int -> Bool
-intLt = undefined
+intLt m n = case m `intCmp` n of
+  LT -> True
+  _ -> False
 
 infixl 6 .+., .-.
 -- У меня это единственный страшный терм во всём файле
 (.+.) :: Int -> Int -> Int
-n .+. m = undefined
+(Pos n) .+. (Pos m) = Pos $ n +. m
+(Neg n) .+. (Neg m) = Neg $ Succ $ n +. m
+(Neg m) .+. (Pos Zero) = Neg m
+(Neg Zero) .+. (Pos (Succ n)) = Pos n
+(Neg (Succ m)) .+. (Pos (Succ n)) = (Neg m) .+. (Pos n)
+m .+. n = n .+. m
 
 (.-.) :: Int -> Int -> Int
 n .-. m = n .+. (intNeg m)
 
 infixl 7 .*.
 (.*.) :: Int -> Int -> Int
-n .*. m = undefined
-
+(Pos m) .*. (Pos n) = Pos $ m *. n
+m .*. (Neg n) = intNeg $ m .*. (intNeg $ Neg n)
+m .*. n = n .*. m
 -------------------------------------------
 -- Рациональные числа
 
-data Rat = Rat Int Nat
+data Rat = Rat Int Nat deriving (Show)
 
 ratNeg :: Rat -> Rat
 ratNeg (Rat x y) = Rat (intNeg x) y
 
 -- У рациональных ещё есть обратные элементы
 ratInv :: Rat -> Rat
-ratInv = undefined
+ratInv (Rat (Pos a) b) = Rat (Pos b) a
+ratInv x = ratNeg $ ratInv $ ratNeg x
 
 -- Дальше как обычно
 ratCmp :: Rat -> Rat -> Tri
-ratCmp = undefined
+ratCmp (Rat x y) (Rat a b) =
+  let y' = Pos y
+      b' = Pos b in
+  (x .*. b') `intCmp` (a .*. y')
 
 ratEq :: Rat -> Rat -> Bool
-ratEq = undefined
+ratEq m n = case m `ratCmp` n of
+  EQ -> True
+  _ -> False
 
 ratLt :: Rat -> Rat -> Bool
-ratLt = undefined
+ratLt m n = case m `ratCmp` n of
+  LT -> True
+  _ -> False
 
 infixl 7 %+, %-
 (%+) :: Rat -> Rat -> Rat
-n %+ m = undefined
+(Rat x y) %+ (Rat a b) = Rat p q where
+  p = (Pos b) .*. x .+. (Pos y) .*. a
+  q = b *. y
 
 (%-) :: Rat -> Rat -> Rat
 n %- m = n %+ (ratNeg m)
 
 infixl 7 %*, %/
 (%*) :: Rat -> Rat -> Rat
-n %* m = undefined
+(Rat x y) %* (Rat a b) = Rat p q where
+  p = x .*. a
+  q = y *. b
 
 (%/) :: Rat -> Rat -> Rat
 n %/ m = n %* (ratInv m)
@@ -207,9 +263,11 @@ n %/ m = n %* (ratInv m)
 -- Определены здесь, но использовать можно и выше
 
 infixr 9 .
+(.) :: (b -> c) -> (a -> b) -> a -> c 
 f . g = \ x -> f (g x)
 
 infixr 0 $
+($) :: (a -> b) -> a -> b
 f $ x = f x
 
 -- Эквивалентные определения
