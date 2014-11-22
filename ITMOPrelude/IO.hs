f51549ffe603bb2edbea4877ef16a0aeb308694a
Anton Evdokimov <free0u@gmail.com>
2014-11-22 17:53:57 +0300

    Added IO states

--- ITMOPrelude/IO.hs
+++ ITMOPrelude/IO.hs
@@ -5,18 +5,30 @@ import ITMOPrelude.Primitive
 import ITMOPrelude.List
 import ITMOPrelude.Categories
 
+import Prelude (Show,Read)
+
 data RealWorld = RealWorld
     { stdIn :: List Nat
     , stdOut :: List Nat
-    , exitCode :: Nat }
+    , exitCode :: Nat } deriving (Show)
 
 type IO a = State RealWorld a
 
 getNat :: IO Nat
-getNat = ?
+getNat = State $ \st -> case st of
+  RealWorld (Cons x input) output code ->
+    (RealWorld input output code, x)
+  RealWorld Nil output code ->
+    undefined
 
 putNat :: Nat -> IO ()
-putNat = ?
+putNat x = State $ \st -> case st of
+  RealWorld input output code ->
+    (RealWorld (Cons x input) output code, ())
 
 setExitCode :: Nat -> IO ()
-setExitCode = ?
+setExitCode nc = State $ \st -> case st of
+  RealWorld input output _ ->
+    (RealWorld input output nc, ())
+
+
